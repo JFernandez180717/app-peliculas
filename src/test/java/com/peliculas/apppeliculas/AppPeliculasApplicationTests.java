619d0c75e0c78adb0713bc59e5086077c13fdf2d
JFernandez180717 <cta.cv2014@gmail.com>
2024-03-10 07:03:30 -0500

    first commit

---
+++ src/test/java/com/peliculas/apppeliculas/AppPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peliculas.apppeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
